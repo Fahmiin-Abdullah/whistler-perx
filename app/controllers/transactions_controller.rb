4055920049985685970f456794cbb7a1794b5396
fahmiin <fahmiin@seedly.sg>
2022-07-01 00:01:11 +0800

    feature: added transactions controller and points service

---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,28 @@
+class TransactionsController < ApplicationController
+  before_action :set_user
+  skip_before_action :verify_authenticity_token
+
+  def create
+    points = PointsService.new(transaction_params[:currency], transaction_params[:amount]).calculate_points
+    @user.with_lock do
+      Transaction.create!(transaction_params)
+      @user.points_cached += points
+      @user.save!
+    end
+
+    render json: 'Transaction processed successfully!', status: :ok
+  end
+
+  private
+
+  def set_user
+    @user = User.find(transaction_params[:user_id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'User not found' }, status: :not_found
+  end
+
+  def transaction_params
+    params.require(%i[user_id currency amount])
+    params.permit(:user_id, :currency, :amount, :description)
+  end
+end
---
+++ app/service/points_service.rb
@@ -0,0 +1,13 @@
+class PointsService
+  def initialize(currency, amount)
+    @currency = currency
+    @amount = amount.to_f
+  end
+
+  def calculate_points
+    standard_points = (@amount / 100).floor * 10
+    return standard_points if @currency.upcase == 'SGD'
+
+    standard_points * 2
+  end
+end
